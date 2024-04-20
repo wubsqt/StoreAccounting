443bb510da48708a1f4a39fdf629141f775d60ea
Mahdi Mohseni <cardinal400@gmail.com>
2024-04-20 17:55:46 +0330

    create models from database

---
+++ StoreAccounting.DataLayer/Context/StoreDBManager.cs
@@ -0,0 +1,59 @@
+﻿using StoreAccounting.DataLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreAccounting.DataLayer.Context
+{
+    public class StoreDBManager : IDisposable
+    {
+        StoreAccounting_DBEntities db;
+        public StoreDBManager(StoreAccounting_DBEntities context)
+        {
+            db = context;
+        }
+
+        private StoreRepository<Employees> _employeeRepository;
+        public StoreRepository<Employees> EmployeeRepository
+        {
+            get
+            {
+                if (_employeeRepository == null)
+                    _employeeRepository = new StoreRepository<Employees>(db);
+
+                return _employeeRepository;
+            }
+        }
+
+        private StoreRepository<Customers> _customerRepository;
+        public StoreRepository<Customers> CustomerRepository
+        {
+            get
+            {
+                if (_customerRepository == null)
+                    _customerRepository = new StoreRepository<Customers>(db);
+
+                return _customerRepository;
+            }
+        }
+
+        private StoreRepository<Products> _productRepository;
+        public StoreRepository<Products> ProductRepository
+        {
+            get
+            {
+                if (_productRepository == null)
+                    _productRepository = new StoreRepository<Products>(db);
+
+                return _productRepository;
+            }
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
