519487ee32f0690a57506108294705abdcda1395
Mahdi Mohseni <cardinal400@gmail.com>
2024-05-11 22:48:49 +0330

    Avg for fill the chart in main window

---
+++ StoreAccounting.Business/ChartFill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreAccounting.Business
+{
+    public static class ChartFill
+    {
+        public static double AvgCalc(List<double> values)
+        {
+            return values.Sum() / values.Count;
+        }
+    }
+}
